dea01d76db8a032b8d7938c21cad22625be42250
suzuki071 <shinee.0711.st@gmail.com>
2021-11-22 06:55:30 +0000

    四則演算テスト作成

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -4,9 +4,20 @@ describe '四則演算' do
       expect(1+1).to eq 2
     end
   end
-  context '足し算' do
-    it '1 + 1 は 2 になる' do
-      expect(1 + 1).to eq 3
+  context '引き算' do
+    it '2 - 1 は 1 になる' do
+      expect(2 - 1).to eq 1
     end
   end
+  context '掛け算' do
+    it '2x1は2になる' do
+      expect(2*1).to eq 2
+    end
+  end
+  context '割り算' do
+    it '2 ÷ 1 は 2 になる' do
+      expect(2 / 1).to eq 2
+    end
+  end
+  
 end
