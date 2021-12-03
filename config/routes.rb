e261937ee74a8fb0f68d4c41827d2ded33aad9ed
suzuki071 <shinee.0711.st@gmail.com>
2021-12-03 10:31:15 +0000

    静的解析追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'todolists/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   get 'top' => 'homes#top'
   post 'todolists' => 'todolists#create'
   get 'todolists' => 'todolists#index'
@@ -9,5 +9,4 @@ Rails.application.routes.draw do
   get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
   patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
   delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
-  
 end
--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,23 +1,25 @@
 describe '四則演算' do
   context '足し算' do
     it '1+1は2になる' do
-      expect(1+1).to eq 2
+      expect(1 + 1).to eq 2
     end
   end
+
   context '引き算' do
     it '2 - 1 は 1 になる' do
       expect(2 - 1).to eq 1
     end
   end
+
   context '掛け算' do
     it '2x1は2になる' do
-      expect(2*1).to eq 2
+      expect(2 * 1).to eq 2
     end
   end
+
   context '割り算' do
     it '2 ÷ 1 は 2 になる' do
       expect(2 / 1).to eq 2
     end
   end
-  
 end
